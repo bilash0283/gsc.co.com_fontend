4243a3f7fb165640c167a4becf4f5b07276b17f5
bilash0283 <bilash0283@gmail.com>
2025-01-07 17:58:20 +0600

    html to php covert

---
+++ pages/include/header.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Global Study Contacts</title>
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+
+  <!-- Favicons -->
+  <link href="assets/src_gsc/favicon2.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com" rel="preconnect">
+  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Jost:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/css/main.css" rel="stylesheet">
+
+</head>
+
+<body class="index-page">
+  <header id="header" class="header d-flex align-items-center fixed-top p-0">
+    <div class="container-fluid container-xl position-relative d-flex align-items-center">
+
+      <a href="index.php" class="logo d-flex align-items-center me-auto">
+        <img src="assets/src_gsc/favicon2.png" alt="">
+        <!-- <h1 class="sitename">Arsha</h1> -->
+      </a>
+
+      <nav id="navmenu" class="navmenu">
+        <ul>
+          <li><a href="#hero" class="active fw-semibold text-uppercase">Home</a></li>
+          <li class="dropdown"><a href="#"><span class="fw-semibold text-uppercase">Services</span> <i
+                class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>
+              <li><a href="student.php" class="fw-semibold text-uppercase">STUDENTS</a></li>
+              <li><a href="#" class="fw-semibold text-uppercase">INSTITUTES</a></li>
+              <li><a href="#" class="fw-semibold text-uppercase">AGENTS</a></li>
+              <li><a href="#" class="fw-semibold text-uppercase">SERVICE PROVIDERS</a></li>
+            </ul>
+          </li>
+          <li class="dropdown"><a href="#"><span class="fw-semibold text-uppercase">Study Destinations</span> <i
+                class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>
+              <li><a href="#" class="fw-semibold text-uppercase">UK</a></li>
+              <li><a href="#" class="fw-semibold text-uppercase">USA</a></li>
+              <li><a href="#" class="fw-semibold text-uppercase">CANADA</a></li>
+              <li><a href="#" class="fw-semibold text-uppercase">AUSTRALIA</a></li>
+              <li><a href="#" class="fw-semibold text-uppercase">EUROPE</a></li>
+            </ul>
+          </li>
+          <li><a href="#team" class="fw-semibold text-uppercase">ABOUT US</a></li>
+          <li class="dropdown"><a href="#" class="fw-semibold text-uppercase"><span>Resources</span> <i
+                class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>
+              <li><a href="#" class="fw-semibold text-uppercase">Student Guide</a></li>
+              <li><a href="#" class="fw-semibold text-uppercase">Scholarship</a></li>
+              <li><a href="#" class="fw-semibold text-uppercase">Study Abroad FAQ</a></li>
+              <li><a href="#" class="fw-semibold text-uppercase">SOP IELTS GRE</a></li>
+              <li><a href="#" class="fw-semibold text-uppercase">Agents Training</a></li>
+              <li><a href="#" class="fw-semibold text-uppercase">Events/Fairs</a></li>
+            </ul>
+          </li>
+          <li><a href="#singup" class="fw-semibold text-uppercase">EVENT</a></li>
+          <li><a href="https://gsc.co.com/apply-agent/portal/index.php" class="fw-semibold text-uppercase">Sign Up / Sing in</a></li>
+          <!-- <li><a href="#contact" class="fw-semibold text-uppercase">login</a></li> -->
+        </ul>
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+      </nav>
+    </div>
+  </header>
