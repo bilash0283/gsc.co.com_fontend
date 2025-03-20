6ce5ad6a0446e0f7a791ba04173b596b4b86a1e0
bilash0283 <bilash0283@gmail.com>
2025-03-20 13:01:14 +0600

    header all social medai add

--- pages/include/header.php
+++ pages/include/header.php
@@ -8,6 +8,40 @@
   <meta name="description" content="Global Study Contacts offers education consultancy,study abroad, visa processing, job scouting, and more to help you start your academic journey abroad,GSC,Student Visa Processing.">
   <meta name="keywords" content="GSC, Global Study Contacts, study abroad, student visa, visa processing, education consultancy, job scouting, overseas education, international study, scholarship assistance, academic counseling, study visa application, study in USA, study in Canada, study in UK, study in Cyprus, study in Denmark, study in Australia, immigration support, university admission guidance, foreign education, international student support, higher education consultancy, student visa consultant, abroad education programs, best study destinations, visa application assistance, global university admission, student scholarship opportunities">
   <meta name="google-site-verification" content="LcfL3ykQzogmP5PldMsiNHqMACOB_KJ_ukt2EAURdUU" />
+  <meta name="author" content="Global Study Contacts Team">
+  <meta property="og:title" content="Global Study Contacts - Study Abroad & Visa Processing">
+  <meta property="og:description" content="Start your academic journey with GSC. We provide consultancy for study abroad, visa processing, and job scouting.">
+  <meta property="og:image" content="https://gsc.co.com/assets/logo.png">
+  <meta property="og:url" content="https://gsc.co.com">
+  <meta property="og:type" content="website">
+
+  <!-- Facebook & LinkedIn (Open Graph) -->
+  <meta property="og:title" content="Global Study Contacts - Study Abroad & Visa Processing">
+  <meta property="og:description" content="Achieve your dreams of studying abroad with GSC. Expert visa support, education consultancy, and international study guidance.">
+  <meta property="og:image" content="https://gsc.co.com/assets/logo.png">
+  <meta property="og:url" content="https://gsc.co.com">
+  <meta property="og:type" content="website">
+  <meta property="og:site_name" content="Global Study Contacts">
+  <meta property="og:locale" content="en_US">
+
+  <!-- Facebook Page Information -->
+  <meta property="fb:app_id" content="YOUR_FACEBOOK_APP_ID">
+  <meta property="article:author" content="https://www.facebook.com/GSCStudyAbroad/">
+
+  <!-- Twitter -->
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:title" content="Global Study Contacts - Study Abroad & Visa Processing">
+  <meta name="twitter:description" content="Global Study Contacts offers expert education consultancy, visa processing, and overseas study support. Start your academic journey with us!">
+  <meta name="twitter:image" content="https://gsc.co.com/assets/logo.png">
+  <meta name="twitter:site" content="@StudyContacts">
+  <meta name="twitter:creator" content="@StudyContacts">
+
+  <!-- Instagram (Uses Open Graph) -->
+  <meta property="og:see_also" content="https://www.instagram.com/gsc_study_abroad/">
+
+  <!-- LinkedIn (Uses Open Graph) -->
+  <meta property="og:see_also" content="https://www.linkedin.com/company/global-study-contacts-gsc/">
+
 
   <!-- Favicons -->
   <link rel="icon" href="assets/src_gsc/bg_logo.ico" type="image/x-icon">
@@ -30,10 +64,10 @@
   <style>
     /* User selection disable */
     body {
-        user-select: none;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
+      user-select: none;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
     }
   </style>
 
@@ -63,28 +97,29 @@
           <li class="dropdown"><a href="index.php"><span class="fw-semibold text-uppercase">Study Destinations</span> <i
                 class="bi bi-chevron-down toggle-dropdown"></i></a>
             <ul>
-            <li>
+              <li>
               <li><a href="australia.php" class="fw-semibold text-uppercase">AUSTRALIA</a></li>
               <li><a href="canada.php" class="fw-semibold text-uppercase">CANADA</a></li>
               <li><a href="denmark.php" class="fw-semibold text-uppercase">DENMARK</a>
-              </li><a href="usa.php" class="fw-semibold text-uppercase">USA</a></li>
-              <li><a href="uk.php" class="fw-semibold text-uppercase">UK</a></li>
-            </ul>
+              </li><a href="usa.php" class="fw-semibold text-uppercase">USA</a>
           </li>
-          <li><a href="contacts.php" class="fw-semibold text-uppercase">CONTACT US</a></li>
-          <li class="dropdown"><a href="#" class="fw-semibold text-uppercase"><span>Resources</span> <i
-                class="bi bi-chevron-down toggle-dropdown"></i></a>
-            <ul>
-              <li><a href="index.php" class="fw-semibold text-uppercase">Student Guide</a></li>
-              <li><a href="index.php" class="fw-semibold text-uppercase">Scholarship</a></li>
-              <li><a href="index.php" class="fw-semibold text-uppercase">Study Abroad FAQ</a></li>
-              <li><a href="index.php" class="fw-semibold text-uppercase">SOP IELTS GRE</a></li>
-              <li><a href="index.php" class="fw-semibold text-uppercase">Agents Training</a></li>
-            </ul>
-          </li>
-          <li><a href="event.php" class="fw-semibold text-uppercase">EVENT</a></li>
-          <li><a href="https://ci-gsc.com" class="fw-semibold text-uppercase">Sign Up / Sign in</a></li>
-          <!-- <li><a href="#contact" class="fw-semibold text-uppercase">login</a></li> -->
+          <li><a href="uk.php" class="fw-semibold text-uppercase">UK</a></li>
+        </ul>
+        </li>
+        <li><a href="contacts.php" class="fw-semibold text-uppercase">CONTACT US</a></li>
+        <li class="dropdown"><a href="#" class="fw-semibold text-uppercase"><span>Resources</span> <i
+              class="bi bi-chevron-down toggle-dropdown"></i></a>
+          <ul>
+            <li><a href="index.php" class="fw-semibold text-uppercase">Student Guide</a></li>
+            <li><a href="index.php" class="fw-semibold text-uppercase">Scholarship</a></li>
+            <li><a href="index.php" class="fw-semibold text-uppercase">Study Abroad FAQ</a></li>
+            <li><a href="index.php" class="fw-semibold text-uppercase">SOP IELTS GRE</a></li>
+            <li><a href="index.php" class="fw-semibold text-uppercase">Agents Training</a></li>
+          </ul>
+        </li>
+        <li><a href="event.php" class="fw-semibold text-uppercase">EVENT</a></li>
+        <li><a href="https://ci-gsc.com" class="fw-semibold text-uppercase">Sign Up / Sign in</a></li>
+        <!-- <li><a href="#contact" class="fw-semibold text-uppercase">login</a></li> -->
         </ul>
         <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
       </nav>
